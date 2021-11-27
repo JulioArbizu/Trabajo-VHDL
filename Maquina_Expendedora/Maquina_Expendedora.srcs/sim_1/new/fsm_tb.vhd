2a7385afab17333cf9ed38970ce532f3b6a68746
Adrofe <adrofe6@gmail.com>
2021-11-27 13:46:24 +0100

    Creacion fsm_tb + reset
    Se crean la entidad top y la maquina de estados (fsm). Se crea tambien la primera version testbench de la fsm, que comprobará el reset

--- Maquina_Expendedora/Maquina_Expendedora.srcs/sim_1/new/fsm_tb.vhd
+++ Maquina_Expendedora/Maquina_Expendedora.srcs/sim_1/new/fsm_tb.vhd
@@ -1,23 +1,14 @@
-----------------------------------------------------------------------------------
--- Ingenieros: Adrian Rodríguez Fernández y Julio Arbizu García
--- Create Date: 27.11.2021 10:46:32
--- Design Name: top
--- Module Name: top - Behavioral
--- Project Name: Maquina Expendedora
--- Description: 
--- Version 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
---library UNISIM;
---use UNISIM.VComponents.all;
 
-entity top is
+
+entity fsm_tb is
+end fsm_tb;
+
+architecture Behavioral of fsm_tb is
+COMPONENT fsm is
     Port (
         CLK                 : in std_logic;                     -- Señal de reloj
         reset_n             : in std_logic;                     -- Entrada reset activa a nivel bajo
@@ -38,11 +29,54 @@ entity top is
         led_dinero_dev      : out std_logic;                     -- true si se ha devuelto correctamente el pago
         led_standby         : out std_logic                     -- true si se esta en el estado de standby
      );
-end top;
-
-architecture Behavioral of top is
-
+end COMPONENT;
+    signal clk : std_logic := '0';
+    signal reset_n, boton_central, moneda_10c, moneda_20c, moneda_50c, moneda_1e,sw_pago : std_logic;
+    signal tarjeta, led_pro_entregado, led_pro_ok, led_trabajando, led_dinero_dev, led_standby : std_logic;
+    signal producto_SW : std_logic_vector(3 downto 0);
+    signal display, an: std_logic_vector(6 downto 0);
+    signal led_cant_dinero : std_logic_vector(9 downto 0);
+       
+    constant CLK_PERIOD : time := 10 ns;    
 begin
+
+genclk : clk <= not clk after 0.5 * CLK_PERIOD;
+
+uut: fsm port map (
+        CLK                 => clk,
+        reset_n             => reset_n,
+        boton_central       => boton_central,
+        moneda_10c          => moneda_10c,
+        moneda_20c          => moneda_20c,
+        moneda_50c          => moneda_50c,
+        moneda_1e           => moneda_1e,
+        tarjeta             => tarjeta,
+        producto_SW         => producto_SW,
+        sw_pago             => sw_pago,
+        display             => display,
+        an                  => an,
+        led_cant_dinero     => led_cant_dinero,
+        led_pro_entregado   => led_pro_entregado,
+        led_pro_ok          => led_pro_ok,
+        led_trabajando      => led_trabajando,
+        led_dinero_dev      => led_dinero_dev,
+        led_standby         => led_standby
+);
+
+estimulos : process 
+begin
+    reset_n <= '0' after 0.25 * CLK_PERIOD, '1' after 0.75 * CLK_PERIOD;
+    wait for 0.1 * CLK_PERIOD;
+    assert led_trabajando = '1'
+        report "[FALLO] Reset no funciona correctamente"
+        severity failure;
+    
+    
+
+    assert false
+        report "[EXITO]: Simulacion Completada"
+        severity failure;
+    end process;
 
 
 end Behavioral;
--- Maquina_Expendedora/Maquina_Expendedora.srcs/sources_1/new/fsm.vhd
+++ Maquina_Expendedora/Maquina_Expendedora.srcs/sources_1/new/fsm.vhd
@@ -1,24 +1,8 @@
-----------------------------------------------------------------------------------
--- Ingenieros: Adrian Rodríguez Fernández y Julio Arbizu García
--- Create Date: 27.11.2021 10:46:32
--- Design Name: top
--- Module Name: top - Behavioral
--- Project Name: Maquina Expendedora
--- Description: 
--- Version 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.std_logic_1164.all;
 
-entity top is
-    Port (
+entity fsm is
+    port (
         CLK                 : in std_logic;                     -- Señal de reloj
         reset_n             : in std_logic;                     -- Entrada reset activa a nivel bajo
         boton_central       : in std_logic;                     -- Utilaza para seleccion del producto y modo de pago con tarjeta
@@ -28,7 +12,6 @@ entity top is
         moneda_1e           : in std_logic;                     -- Moneda 1 euro
         tarjeta             : in std_logic;                     -- Entrada NFC
         producto_SW         : in std_logic_vector(7 downto 0);  -- Switches para seleccion numero de producto en BCD
-        sw_pago             : in std_logic;                     -- Switch para elegir pago con tarjeta (false) o efectivo (true)
         display             : out std_logic_vector(6 downto 0); -- Salida a los displays
         an                  : out std_logic_vector(6 downto 0); -- Anodos de los displays
         led_cant_dinero     : out std_logic_vector(9 downto 0); -- Cantidad de dinero en led, 10 leds encendidos es 1 euro, 0 leds encendido 0 euros
@@ -37,12 +20,62 @@ entity top is
         led_trabajando      : out std_logic;                    -- true si la maquina está procesando, o bien esperando al pago, devolviendo o entregando producto
         led_dinero_dev      : out std_logic;                     -- true si se ha devuelto correctamente el pago
         led_standby         : out std_logic                     -- true si se esta en el estado de standby
-     );
-end top;
+ );
+end fsm;
 
-architecture Behavioral of top is
-
+architecture behavioral of fsm is
+    type STATES is (S0, S1, S2, S3);
+    signal current_state: STATES := S0;
+    signal next_state: STATES;
 begin
+    state_register: process (RESET, CLK)
+    begin
+        if RESET = '0' then
+            current_state <= S0;
+        elsif rising_edge(clk) then
+            current_state <= next_state;
+        end if;
+    end process;
 
-
-end Behavioral;
+    nextstate_decod: process (PUSHBUTTON, current_state)
+    begin
+        next_state <= current_state;
+        case current_state is
+           when S0 =>
+             if PUSHBUTTON = '1' then
+             next_state <= S1;
+             end if;
+           when S1 =>
+             if PUSHBUTTON = '1' then
+             next_state <= S2;
+             end if;
+           when S2 =>
+             if PUSHBUTTON = '1' then
+             next_state <= S3;
+             end if;
+           when S3 =>
+             if PUSHBUTTON = '1' then
+             next_state <= S0;
+             end if;
+           when others =>
+             next_state <= S0;
+        end case;
+    end process;
+    
+    output_decod: process (current_state)
+    begin
+        LIGHT <= (OTHERS => '0');
+        case current_state is
+         when S0 =>
+            LIGHT(0) <= '1';
+         when S1 =>
+            LIGHT(1) <= '1';
+         when S2 =>
+            LIGHT(2) <= '1';
+         when S3 =>
+            LIGHT(3) <= '1';
+         when others =>
+            LIGHT <= (OTHERS => '0');
+        end case;
+    end process;
+end behavioral;
