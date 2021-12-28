9daf32d9a255e4f15c14989c23f4e131ec85d31e
Julio A. Arbizu García <Julioarbizug@gmail.com>
2021-12-28 15:05:40 +0100

    divisor reloj 2Hz

---
+++ Maquina_Expendedora/Maquina_Expendedora.srcs/sim_1/new/divisorReloj_tb.vhd
@@ -0,0 +1,53 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY divisorReloj_tb IS
+END divisorReloj_tb;
+
+ARCHITECTURE behavior OF divisorReloj_tb IS
+
+ -- Component Declaration for the Unit Under Test (UUT)
+
+COMPONENT Clock_Divider
+PORT(
+clk,reset : IN std_logic;
+clock_out : OUT std_logic
+);
+END COMPONENT;
+
+--Inputs
+signal clk,reset : std_logic := '0';
+
+--Outputs
+signal clock_out : std_logic;
+
+ -- Clock period definitions
+constant clk_period : time := 100 us;
+
+BEGIN
+
+ -- Instantiate the Unit Under Test (UUT)
+uut: Clock_Divider PORT MAP (
+clk => clk,
+clock_out => clock_out,
+reset => reset
+);
+
+ -- Clock process definitions
+clk_process :process
+begin
+reset <= '1';
+clk <= '0';
+wait for clk_period/2;
+clk <= '1';
+wait for clk_period/2;
+end process;
+
+ -- Stimulus process
+stim_proc: process
+begin
+
+wait;
+end process;
+
+END;
